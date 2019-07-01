523b4ab4b410ca6880acb0583c513a47020a0afe
TheGreekCuban <theodorealexander92@gmaill.com>
2019-07-01 14:34:42 -0400

    just need to fix grid

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 //We need an array of countries that we will use to dynamically create buttons
 arrayOfCountries = ["America", "Canada", "Mexico", "Greece", "Cuba", "Chile", "Peru", "Switzerland", "France", "Spain",
-"China", "Russia", "Vietnam"]
+"China", "Vietnam"]
 
 //We need a function that dynamically creates all these buttons on page load
-window.onload =  function () {
-
+window.onload = () => {
     for (let i = 0; i < arrayOfCountries.length; i++) {
       let currentCountry = arrayOfCountries[i]
       let buttonDiv = document.createElement('button')
+
+      //add click handler to buttons. Put an anonymous function before it so that it can be blocked from calling.
+      buttonDiv.onclick = e => urlBuilderButton(e, currentCountry)
 
       //set id of dynamicButtons
       buttonDiv.setAttribute("id", "dynamicButtons")
@@ -21,7 +23,7 @@ window.onload =  function () {
       //set text of button
       buttonDiv.innerHTML = currentCountry
 
-      //append button to the buttons div
+    //append button to the buttons div
     document.getElementById("buttonsDiv").appendChild(buttonDiv)
   }
 }
@@ -97,37 +99,65 @@ const addButtons = () => {
 
   //push the userSearch into the array 
   arrayOfCountries.push(userSearch)
+  //console.log("user search 1: ", userSearch)
 
   //create a button div and set the matching attributes
   let buttonDiv = document.createElement('button')
   buttonDiv.setAttribute('country', userSearch)
   buttonDiv.setAttribute('id', 'dynamicButtons')
-  buttonsDiv.setAttribute('calss', 'buttonsDiv')
+  buttonDiv.setAttribute('calss', 'buttonsDiv')
   buttonDiv.innerText = userSearch
+  buttonDiv.onclick = e => urlBuilderButton(e, userSearch)
 
   //append button to buttons div
   document.getElementById('buttonsDiv').appendChild(buttonDiv)
-
-  //This function will set the value of the button clicked to the query parameter search term
-  document.getElementById("dynamicButtons").onclick = urlBuilderButton()  
 }
 
 //We need a function that will run an api call from the button clicked
-const urlBuilderButton = () => {
+const urlBuilderButton = (event, userSearch) => {
+    event.preventDefault();
     
     let queryParams = {
         giphyURL: 'https://api.giphy.com/v1/gifs/search?',
         apiKey: 'api_key=k8QvgjlfZJisxH81TEc50Xxb5ZgErQ5i&q=',
         urlEnd: '&limit=3&offset=0&rating=R&lang=en'
     }
-
-    let buttonSearch = document.getElementById("buttonsDiv").getAttribute("country")
-    console.log("User Search: ", buttonSearch)
+    
+    console.log("User Search 2: ", userSearch)
 
     //create the final URL:
-    let finalURL = `${queryParams.giphyURL}${queryParams.apiKey}${buttonSearch}${queryParams.urlEnd}`
-    console.log("URL: ", finalURL)
-    return finalURL
-}  
+    let finalURL = `${queryParams.giphyURL}${queryParams.apiKey}${userSearch}${queryParams.urlEnd}`
+    
+    fetch(finalURL).then(function(response) {
+        return response.json();
+    }).then(function(myJson) {
 
-console.log(document.getElementById('test').getAttribute("country"))
+        //Must loop through the array in the object then dynamically create a p div and image div for each gif.
+        for (let i = 0; i < myJson.data.length; i++) {
+        
+        //We have to dynamically create a div to append all gifs into
+        let innerAppendDiv = document.createElement('div')
+
+        //We must set the text of the p to rating
+        let innerPDiv = document.createElement('p')
+        let rating = myJson.data[i].rating
+        innerPDiv.innerHTML = `Rating: ${rating}`
+
+        //We must set the src attribute to the image url of the dynamically created image div
+        let imageURL = myJson.data[i].images.original.url
+        let innerPicDiv = document.createElement('img')
+
+        innerPicDiv.setAttribute('src', imageURL)
+        innerPicDiv.style.width = "150px"
+        innerPicDiv.style.height = "150px"
+
+        //We must appen those to the innerDiv
+        innerAppendDiv.append(innerPDiv)
+        innerAppendDiv.append(innerPicDiv)
+
+        //Once done with the loop we must prepend the innerDiv to the page 
+        document.getElementById('pictures').prepend(innerAppendDiv)   
+        }
+        clear()
+    });   
+}
